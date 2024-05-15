c3973d168bbd63141cd35e3de0af04ade0d27f04
jerry <jerrycai@shein.com>
2024-05-15 21:17:48 +0800

    feat: data resource block parse

--- src/sections/image_resources_section.rs
+++ src/sections/image_resources_section.rs
@@ -1,19 +1,72 @@
+use std::ops::Range;
+
 use thiserror::Error;
 
+use crate::sections::psd_cursor::PSDCursor;
+
+const EXPECTED_RESOURCE_BLOCK_SIGNATURE: [u8; 4] = [56, 66, 73, 77];
 
 #[derive(Debug, Error)]
 pub enum ImageResourcesSectionError {
-
+    #[error(
+        r#"The first four bytes (indices 0-3) must always equal [56, 66, 73, 77],
+         which in string form is '8BIM'."#
+    )]
+    InvalidSignature,
 }
 
 #[derive(Debug)]
-pub struct ImageResourcesSection {
-
-}
+pub struct ImageResourcesSection {}
 
 impl ImageResourcesSection {
-  pub fn from_bytes(bytes: &[u8]) -> Result<Self, ImageResourcesSectionError> {
+    pub fn from_bytes(bytes: &[u8]) -> Result<Self, ImageResourcesSectionError> {
+        let mut cursor = PSDCursor::new(bytes);
 
-    unimplemented!()
-  }
+        // https://www.adobe.com/devnet-apps/photoshop/fileformatashtml/#50577409_69883
+        // The first 4 bytes are the length of the image resource section.
+        let length = cursor.read_4bytes_as_u32() as u64;
+
+        while cursor.position() < length {
+            let block = Self::read_resource_block(&mut cursor);
+        }
+
+        unimplemented!()
+    }
+
+    fn read_resource_block(
+        cursor: &mut PSDCursor,
+    ) -> Result<ImageResourceBlock, ImageResourcesSectionError> {
+        let signature = cursor.read_4bytes();
+        if signature != EXPECTED_RESOURCE_BLOCK_SIGNATURE {
+            return Err(ImageResourcesSectionError::InvalidSignature);
+        }
+
+        let resource_id = cursor.read_2bytes_as_i16();
+
+        let name = cursor.read_pascal_string();
+
+        // Actual size of resource data that follows
+        let data_len = cursor.read_4bytes_as_u32();
+        // Data length is padded to make the size even.
+        let data_len = data_len + data_len % 2;
+        let start = cursor.position() as usize;
+        let end = start + data_len as usize;
+        let data_range = Range {
+          start,
+          end
+        };
+        cursor.read(data_len);
+
+        Ok(ImageResourceBlock {
+          resource_id,
+          name,
+          data_range
+        })
+    }
+}
+
+struct ImageResourceBlock {
+    resource_id: i16,
+    name: String,
+    data_range: Range<usize>,
 }
--- src/sections/psd_cursor.rs
+++ src/sections/psd_cursor.rs
@@ -36,19 +36,27 @@ impl<'a> PSDCursor<'a> {
     bytes
   }
 
+  /// Read 2 bytes
   pub fn read_2bytes(&mut self) -> &[u8] {
     self.read(2)
   }
 
+  /// Read 4 bytes
   pub fn read_4bytes(&mut self) -> &[u8] {
     self.read(4)
   }
 
+  /// Read 6 bytes
   pub fn read_6bytes(&mut self) -> &[u8] {
     self.read(6)
   }
 
-  /// Read 2 bytes as u16
+  /// Read 1 byte as a u8
+  pub fn read_1byte_as_u8(&mut self) -> u8 {
+    self.read(1)[0]
+  }
+
+  /// Read 2 bytes as a u16
   pub fn read_2bytes_as_u16(&mut self) -> u16 {
     let bytes = self.read_2bytes();
 
@@ -58,10 +66,37 @@ impl<'a> PSDCursor<'a> {
     u16::from_be_bytes(array)
   }
 
+  /// Read 2 bytes as a i16
+  pub fn read_2bytes_as_i16(&mut self) -> i16 {
+    let bytes = self.read_2bytes();
+
+    let mut array = [0; 2];
+    array.copy_from_slice(bytes);
+
+    i16::from_be_bytes(array)
+  }
+
   /// Read 4 bytes as a u32
   pub fn read_4bytes_as_u32(&mut self) -> u32 {
     let bytes = self.read_4bytes();
     u32_from_be_bytes(bytes)
+  }
+
+  /// Reads 'Pascal string'
+  /// 
+  /// Pascal string is UTF-* string, padded to make the size even
+  /// (a null name consists of two bytes of 0)
+  pub fn read_pascal_string(&mut self) -> String {
+    let len = self.read_1byte_as_u8();
+    let data = self.read(len as u32);
+    let result = String::from_utf8_lossy(data).into_owned();
+
+    if len % 2 == 0 {
+      // If the total length is odd, read an extra null byte
+      self.read_1byte_as_u8();
+    }
+
+    result
   }
 }
 
